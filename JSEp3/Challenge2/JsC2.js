2a4535d2d468421c94e22eed2476ddc8894f425e
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-05-17 17:05:18 +0800

    Add files via upload

---
+++ JSEp3/Challenge2/JsC2.js
@@ -0,0 +1,9 @@
+
+let dbMath = 94.24;
+let prog = 90.47;
+let dStructures = 88.95;
+let dBase = 98.52;
+let Ehacking = 96.82;
+let average = ((dbMath + prog + dStructures + dBase + Ehacking) / 5);
+
+alert("Total Average : " + average.toFixed(2)); // .toFixed to remove excess decimals. 
---
+++ JSEp3/JSEp3.js
@@ -0,0 +1,37 @@
+let num = parseInt("5");
+// alert(num);
+let pi = parseFloat("5.53");
+
+// alert(pi);
+
+// let cal = num + pi;
+
+// alert(cal)
+
+let num1 = "5";
+let num2 = 10;
+
+alert("Output of 5 + 10 = " + (parseInt(num1) + num2));
+
+let x = 5;
+let y = 10;
+alert("Output of 5 + 10 = 15 / 5 = " + ((x+y)/x)); 
+
+let a = 12;
+let b = 3;
+alert("Output of 12 / 3 = 4 + 12 = " + ((a/b+a)));
+
+let number1 = 5;
+let number2 = 2;
+let number3 = 7;
+alert((number1 - number3) + number2 ** number2); 
+
+let c = 20;
+c += 10;
+alert(c);
+
+let d = 8;
+alert(d++);
+alert(d);
+
+ 
