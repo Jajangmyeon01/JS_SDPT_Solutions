d16d487b5fe5c5b2b1b4b19c1031cb90cb4ff016
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-05-17 17:05:08 +0800

    Add files via upload

---
+++ JSEp2/Challenge/JsC1.js
@@ -0,0 +1,8 @@
+let s1 = "Gen";
+let s2 = "Anna";
+let gender = "she";
+let drink = "coffee";
+
+alert(s2 + ", the friendly neighbor, waved at " + s1 + " as " + gender + " walked by " + s1 + "'s house. "
+    + s1 + " smiled back and invited " + s2 + " in for a cup of " + drink + "."
+);
---
+++ JSEp2/JSEP2.js
@@ -0,0 +1,28 @@
+console.log("Hello World");
+
+/*
+let $name = "Gen Exus Vergara";
+alert($name);
+
+$name = "Anna Kendrick";
+alert($name);
+*/
+let num = 10.24;
+alert("Your number is : " + num + ", Am I right?"); 
+
+let islazy = true;
+
+let userName;
+
+let x = 5;
+let y = 10;
+
+let z = x + y;
+
+console.log(z);
+// document.getElementById("text").innerHTML = $name;
+
+
+
+
+
