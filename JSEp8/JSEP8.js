98ab8b04acb7fb6ae62f8cd084a6e0cd3dee0762
Gen Exus Vergara <121187643+Jajangmyeon01@users.noreply.github.com>
2024-05-22 23:17:11 +0800

    Add files via upload

---
+++ JSEp8/JSEP8.js
@@ -0,0 +1,37 @@
+let difficulty = 2;
+
+switch(difficulty){
+    case 1:
+        alert("Easy");
+        break;
+    case 2:
+        alert("Normal");
+        break;
+    case 3: 
+        alert("Hard");
+        break;
+    default:
+        alert("Invalid Difficulty");
+        break;
+}
+
+
+let letter = "a";
+
+switch(letter){
+    case "a":
+    case "A":
+        alert("Letter a/A");
+        break;
+    case "b":
+    case "B":
+        alert("Letter b/B");
+        break;
+    case "c": 
+    case "C":
+        alert("Letter c/C");
+        break;
+    default:
+        alert("Invalid Letter");
+        break;
+}
